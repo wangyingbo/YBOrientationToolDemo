81470431a3b55291e02bb17faaf23edad4184a31
wangyingbo <wangyingbo528@126.com>
2018-09-29 10:47:21 +0800

    init

---
+++ YBOrientationTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YBOrientationTool
+//
+//  Created by 王迎博 on 2018/9/29.
+//  Copyright © 2018年 王颖博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
